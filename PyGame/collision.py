a62903558ee168e06f0c2b176f970f2b3551c0a5
esserafael <esserafael@gmail.com>
2020-04-24 00:41:41 -0300

    Some fixes

--- PyGame/collision.py
+++ PyGame/collision.py
@@ -50,11 +50,12 @@ def draw_block(block: FormData):
         TextRect.center = x,y
         gameDisplay.blit(TextSurf, TextRect)
 
-    block_draw = pygame.draw.rect(gameDisplay, block.color, [block.pos.x, block.pos.y, block.size.width, block.size.height])
+    pygame.draw.rect(gameDisplay, block.color, [block.pos.x, block.pos.y, block.size.width, block.size.height])
 
-    message_display("x:{} y:{}".format(block.center.x, block.center.y), 12,block.center.x, block.center.y)
+    message_display(block.name, 12, block.center.x, block.center.y - 15)
+    message_display("x:{} y:{}".format(block.center.x, block.center.y), 12, block.center.x, block.center.y)
 
-    return block_draw
+    return
 
 def text_objects(text, font):
     textSurface = font.render(text, True, black)
@@ -81,6 +82,13 @@ def game_loop():
     #    other_forms.append(CreatedFormData("block_{}".format(total_forms_generated), random.randrange(0, display_width - 100), random.randrange(0, display_height - 100), 100, 100, black))
     #    total_forms_generated += 1
 
+    main_block = FormData(
+        "main_block", 
+        FormDataPosition(main_form_x, main_form_y), 
+        FormDataSize(main_form_width, main_form_height), 
+        FormDataPosition(round(main_form_x + main_form_width / 2), round(main_form_y + main_form_height / 2)),
+        red)
+
     other_forms = [
         (FormData(
             "block_{}".format(form),
@@ -91,7 +99,7 @@ def game_loop():
             )) 
             for form in range(total_other_forms)]
 
-    #print(other_forms)
+  
 
     while not gameExit:
         for event in pygame.event.get():
@@ -114,22 +122,21 @@ def game_loop():
 
         gameDisplay.fill(white)
 
-        main_form_x += main_form_x_change
-        main_form_y += main_form_y_change
+        main_block.pos = FormDataPosition(main_block.pos.x + main_form_x_change, main_block.pos.y + main_form_y_change)
 
         #"""
-        display_x_boundary = display_width - main_form_width
-        display_y_boundary = display_height - main_form_height
+        display_x_boundary = display_width - main_block.size.width
+        display_y_boundary = display_height - main_block.size.height
 
-        if main_form_x > display_x_boundary:
-            main_form_x = display_x_boundary
-        elif main_form_x < 0:
-            main_form_x = 0
+        if main_block.pos.x > display_x_boundary:
+            main_block.pos.x = display_x_boundary
+        elif main_block.pos.x < 0:
+            main_block.pos.x = 0
 
-        if main_form_y > display_y_boundary:
-            main_form_y = display_y_boundary
-        elif main_form_y < 0:
-            main_form_y = 0
+        if main_block.pos.y > display_y_boundary:
+            main_block.pos.y = display_y_boundary
+        elif main_block.pos.y < 0:
+            main_block.pos.y = 0
         #"""
 
         if other_forms:
@@ -137,14 +144,8 @@ def game_loop():
                 form.center = FormDataPosition(round(form.pos.x + form.size.width / 2), round(form.pos.y + form.size.height / 2))
                 draw_block(form)
 
-        main_block = draw_block(
-            FormData(
-                "main_block", 
-                FormDataPosition(main_form_x, main_form_y), 
-                FormDataSize(main_form_width, main_form_height), 
-                FormDataPosition(round(main_form_x + main_form_width / 2), round(main_form_y + main_form_height / 2)),
-                red
-                ))
+        main_block.center = FormDataPosition(round(main_block.pos.x + main_block.size.width / 2), round(main_block.pos.y + main_block.size.height / 2))
+        draw_block(main_block)        
         
         #print(blocks_generated)
 
